7a5c0bc733ffc41ffe6df4a4ec76c63d217808e7
MrShadowX00 <khujaking@gmail.com>
2021-06-29 19:09:26 +0500

    Mirkhujaev

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -1,10 +1,147 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({Key? key}) : super(key: key);
 
   static const routeName = '/screen_1';
+  static const String title = 'Lorem Ipsum is simply dummy text of '
+      'the printing and typesetting industry. '
+      'Lorem Ipsum has been the industry\'s standard'
+      ' dummy text ever since the 1500s, when an unknown printer';
+
+  static const String longText =
+      'Contrary to popular belief, Lorem Ipsum is not simply random text.'
+      ' It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
+      ' Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, '
+      'looked up one of the more obscure Latin words, consectetur, '
+      'from a Lorem Ipsum passage, and going through the cites of the word in classical literature, '
+      'discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" '
+      '(The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, '
+      'very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",'
+      ' comes from a line in section 1.10.32 There are many variations of passages of'
+      ' Lorem Ipsum available, but the majority have suffered alteration '
+      'in some form, by injected humour, or randomised words which don\'t '
+      'look even slightly believable. If you are going to use a passage '
+      'of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing'
+      ' hidden in the middle of text. All the Lorem Ipsum generators on the Internet '
+      'tend to repeat predefined chunks as necessary, making this the first true '
+      'generator on the Internet. It uses a dictionary of over 200 Latin words, '
+      'combined with a handful of model sentence structures, to generate Lorem '
+      'Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always'
+      ' free from repetition, injected humour, or non-characteristic words etc.';
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.8,
+        width: MediaQuery.of(context).size.width - 20,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                height: 40,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(),
+                    Container(
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Title',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                          alignment: Alignment.topRight,
+                          child: const Text(
+                            'X',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                child: const Text(
+                  '$title',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.black12,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Scrollbar(
+                isAlwaysShown: true,
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        child: const Text(
+                          '$longText',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: Center(
+                  child: GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      height: 50,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.purpleAccent,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'TEST BUTTON',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/screens/screen_2.dart
+++ lib/screens/screen_2.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Screen2 extends StatelessWidget {
@@ -5,6 +8,112 @@ class Screen2 extends StatelessWidget {
 
   static const routeName = '/screen_2';
 
+  Widget singleItemWidget(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: PhysicalModel(
+        color: Colors.black,
+        shadowColor: Colors.black,
+        elevation: 6,
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          height: 130,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          child: Center(
+            child: Text(
+              '$title',
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Container(
+                      alignment: Alignment.topRight,
+                      child: const Text(
+                        'X',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                        ),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  child:const Text(
+                    'Section 1 (2)',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 150,
+                 child:ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder:(BuildContext context, int index){
+                        return singleItemWidget('Horizontal item $index');
+                      }
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  child:const Text(
+                    'Section 2 (4)',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child:ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: 10,
+                    itemBuilder:(BuildContext context, int index){
+                      return singleItemWidget('Horizontal item $index');
+                    }
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
